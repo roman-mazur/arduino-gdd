fa819b0e22325e6ba14a5b37c4e52386769e22d1
Olexandr <rivne2@gmail.com>
2011-10-05 15:36:48 +0000

    Support better communication.

---
+++ eclipse-project/src/com/stanfy/arduino/ArduinoBaseActivity.java
@@ -0,0 +1,185 @@
+package com.stanfy.arduino;
+
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
+
+import com.android.future.usb.UsbAccessory;
+import com.android.future.usb.UsbManager;
+
+public abstract class ArduinoBaseActivity extends Activity implements Runnable {
+  
+  /** Message codes. */
+  public static final int MESSAGE_DISPLAY = 1;
+
+  private static final String TAG = "ArduinoActivity";
+  private static final String ACTION_USB_PERMISSION = "com.stanfy.arduino.action.USB_PERMISSION";
+
+  private UsbManager usbManager;
+  private PendingIntent pendingIntent;
+  private boolean permissionEnabled;
+
+  UsbAccessory usbAccessory;
+
+  FileInputStream inputStream;
+  FileOutputStream outputStream;
+  ParcelFileDescriptor fileDescriptor;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    usbManager = UsbManager.getInstance(getApplicationContext());
+    pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0,
+        new Intent(ACTION_USB_PERMISSION), 0);
+    final IntentFilter intentFilter = new IntentFilter(ACTION_USB_PERMISSION);
+    intentFilter.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
+    registerReceiver(usbReceiver, intentFilter);
+
+    if (getLastNonConfigurationInstance() != null) {
+      usbAccessory = (UsbAccessory) getLastNonConfigurationInstance();
+      openAccessory(usbAccessory);
+      Log.i(TAG, "accessoty opened !!!");
+    }
+  }
+
+  @Override
+  public Object onRetainNonConfigurationInstance() {
+    if (usbAccessory != null) {
+      return usbAccessory;
+    } else {
+      return super.onRetainNonConfigurationInstance();
+    }
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+    if (inputStream != null && outputStream != null) { return; }
+
+    final UsbAccessory[] accessories = usbManager.getAccessoryList();
+    final UsbAccessory accessory = (accessories == null ? null : accessories[0]);
+    if (accessory != null) {
+      if (usbManager.hasPermission(accessory)) {
+        openAccessory(accessory);
+      } else {
+        synchronized (usbReceiver) {
+          if (!permissionEnabled) {
+            usbManager.requestPermission(accessory, pendingIntent);
+            permissionEnabled = true;
+          }
+        }
+      }
+    } else {
+      Log.w(TAG, "Accessory not found");
+    }
+  }
+
+  private final BroadcastReceiver usbReceiver = new BroadcastReceiver() {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+      String action = intent.getAction();
+      if (ACTION_USB_PERMISSION.equals(action)) {
+        synchronized (this) {
+          UsbAccessory accessory = UsbManager.getAccessory(intent);
+          if (intent
+              .getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+            openAccessory(accessory);
+          } else {
+            Log.i(TAG, "Permission for accessory " + accessory + "- denied");
+          }
+          permissionEnabled = false;
+        }
+      } else if (UsbManager.ACTION_USB_ACCESSORY_DETACHED.equals(action)) {
+        UsbAccessory accessory = UsbManager.getAccessory(intent);
+        if (accessory != null && accessory.equals(usbAccessory)) {
+          closeAccessory();
+        }
+      }
+    }
+  };
+
+  private void openAccessory(UsbAccessory accessory) {
+    fileDescriptor = usbManager.openAccessory(accessory);
+    if (fileDescriptor != null) {
+      usbAccessory = accessory;
+      FileDescriptor descriptor = fileDescriptor.getFileDescriptor();
+      inputStream = new FileInputStream(descriptor);
+      outputStream = new FileOutputStream(descriptor);
+      new Thread(this).start();
+      Log.i(TAG, "accessory opened");
+    } else {
+      Log.i(TAG, "accessory closed");
+    }
+  }
+  
+  @Override
+  public void run() {
+    Thread.yield();
+    int ret = 0;
+    final byte[] buffer = new byte[16384];
+
+    while (ret >= 0) {
+      try {
+        ret = inputStream.read(buffer);
+      } catch (IOException e) {
+        Utils.logThrowable(TAG, e);
+        break;
+      }
+      
+      if (ret > 0) {
+        final String str = new String(buffer, 0, ret);
+        getHandler().handleMessage(Message.obtain(getHandler(), MESSAGE_DISPLAY, str));
+//        runOnUiThread(new Runnable() {
+//          
+//          @Override
+//          public void run() {
+//            Toast.makeText(ArduinoBaseActivity.this, str, Toast.LENGTH_SHORT);
+//          }
+//        });
+      }
+    }
+  }
+  
+  protected abstract Handler getHandler();
+
+  private void closeAccessory() {
+    try {
+      if (fileDescriptor != null) {
+        fileDescriptor.close();
+      }
+    } catch (IOException ex) {
+      Log.i(TAG, ex.getMessage());
+    } finally {
+      fileDescriptor = null;
+      usbAccessory = null;
+    }
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    closeAccessory();
+  }
+
+  @Override
+  protected void onDestroy() {
+    unregisterReceiver(usbReceiver);
+    super.onDestroy();
+  }
+}
--- eclipse-project/src/com/stanfy/arduino/ArduinoRequestActivity.java
+++ eclipse-project/src/com/stanfy/arduino/ArduinoRequestActivity.java
@@ -3,65 +3,71 @@ package com.stanfy.arduino;
 import java.io.IOException;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
+import android.os.Message;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class ArduinoRequestActivity extends ArduinoTestingActivity {
+public class ArduinoRequestActivity extends ArduinoBaseActivity implements
+    OnClickListener {
 
-	private Button button;
-	private final byte port = 0x1;
-	private final byte mode = 0x0;
-	private final byte value = 0x1;
-	private final byte portNumber = 0x2;
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		
-		button = (Button) findViewById(R.id.send);
-		button.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				try {
-					if (inputStream != null) {
-					outputStream.write("Hello world".getBytes());
-					outputStream.flush();
-					final StringBuffer buf = new StringBuffer();
-					final byte[] bbuf = new byte[256];
-					if (inputStream.read(bbuf) > 0) {
-						buf.append(new String(bbuf));
-						Toast.makeText(v.getContext(), buf, Toast.LENGTH_LONG).show();
-					}
-					}
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-//				sendByte(port, portNumber, mode, value);
-			}
-		});
-	}
-	
-	public void sendByte(byte portType, byte portNumber, byte mode, byte value) {
-		byte[] bufferBytes = new byte[4];
-		bufferBytes[0] = portType;
-		bufferBytes[1] = portNumber;
-		bufferBytes[2] = mode;
-		bufferBytes[3] = value;
-		
-		if (inputStream != null && bufferBytes[1] != -1) {
-			try {
-				outputStream.write(bufferBytes);
-				outputStream.flush();
-			} catch (IOException ex) {
-				Log.e("ERROR", ex.getMessage());
-			}
-		}
-	}
-	
+  private static final String TAG = "RequestA";
+
+  private Button button;
+  private EditText input;
+  private TextView output;
+
+  private final Handler mesHandler = new Handler() {
+    public void handleMessage(final Message msg) {
+      switch (msg.what) {
+        case MESSAGE_DISPLAY:
+          runOnUiThread(new Runnable() {
+            
+            @Override
+            public void run() {
+              if (msg.obj == null) {
+                output.append("Nothing to display\n");
+              } else {
+                output.append(msg.obj.toString() + "\n");
+              }
+            }
+          });
+          break;
+        default:
+      }
+    }
+  };
+  
+  protected Handler getHandler() { return mesHandler; }
+  
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+
+    button = (Button) findViewById(R.id.send);
+    button.setOnClickListener(this);
+    input = (EditText) findViewById(R.id.edit);
+    output = (TextView) findViewById(R.id.text);
+  }
+
+  @Override
+  public void onClick(final View v) {
+    if (v.getId() == R.id.send) {
+      try {
+        if (inputStream != null && !TextUtils.isEmpty(input.getText())) {
+          outputStream.write(input.getText().toString().getBytes());
+          outputStream.flush();
+          input.setText("");
+        }
+      } catch (final IOException e) {
+        output.setText(Utils.logThrowable(TAG, e) + "\n");
+      }
+    }
+  }
+
 }
