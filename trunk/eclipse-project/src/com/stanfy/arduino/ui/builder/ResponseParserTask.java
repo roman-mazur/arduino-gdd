04e4dd8b9eb6ee0f7fcec0826efd2d19f5d97e19
mazur.roman@gmail.com <mazur.roman@gmail.com@0f16de9c-53e2-40c1-0ac0-dbf10a6d3398>
2011-10-05 17:57:04 +0000

    enhancements

--- trunk/eclipse-project/src/com/stanfy/arduino/ui/builder/ResponseParserTask.java
+++ trunk/eclipse-project/src/com/stanfy/arduino/ui/builder/ResponseParserTask.java
@@ -9,8 +9,14 @@ import android.view.ViewGroup;
  */
 public class ResponseParserTask {
 
+  /** Response separator. */
+  private static final String SEPARATOR = "Router>";
+
   /** Builder. */
   private UIBuilder builder;
+
+  /** Current buffer. */
+  private StringBuilder currentBuffer = new StringBuilder();
 
   /**
    * Commands.
@@ -29,10 +35,30 @@ public class ResponseParserTask {
     this.builder = new UIBuilder(container);
   }
 
+  private String filterString(final String text) {
+    final int index = text.indexOf(SEPARATOR);
+    if (index == -1) {
+      currentBuffer.append(text);
+      return null;
+    }
+    String response = text.substring(0, index);
+    if (currentBuffer.length() > 0) {
+      response = currentBuffer.toString() + response;
+      currentBuffer.delete(0, currentBuffer.length());
+    }
+    if (index < text.length() - SEPARATOR.length() - 1) {
+      currentBuffer.append(text.substring(index + SEPARATOR.length()));
+    }
+    return response;
+  }
+
   public void displayResponse(final Command command, final String response) {
     if (builder == null) { return; }
-    final List<UIDirective> directions = command.parser.parse(response);
-    builder.publishParseResults(directions);
+    final String text = filterString(response);
+    if (text != null) {
+      final List<UIDirective> directions = command.parser.parse(response);
+      builder.publishParseResults(directions);
+    }
   }
 
 }
