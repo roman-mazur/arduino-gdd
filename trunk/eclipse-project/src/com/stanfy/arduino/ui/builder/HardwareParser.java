19657ff2fb9dc82dac04e2f56d6802b93c13d347
mazur.roman@gmail.com <mazur.roman@gmail.com@0f16de9c-53e2-40c1-0ac0-dbf10a6d3398>
2011-10-05 19:18:05 +0000

    parsing tech support link

--- trunk/eclipse-project/src/com/stanfy/arduino/ui/builder/HardwareParser.java
+++ trunk/eclipse-project/src/com/stanfy/arduino/ui/builder/HardwareParser.java
@@ -1,7 +1,15 @@
 package com.stanfy.arduino.ui.builder;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.graphics.Color;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.URLSpan;
 
 /**
  * @author Roman Mazur (Stanfy - http://www.stanfy.com)
@@ -9,9 +17,23 @@ import java.util.List;
  */
 public class HardwareParser implements Parser {
 
+  private static final Pattern TECH_SUPPORT_PATTERN = Pattern.compile("Technical\\sSupport:\\s(http://.+)" + (DEBUG ? "" : "\\n"));
+
+  private static Spannable url(final String label, final String url) {
+    final SpannableString result = new SpannableString(label + ": " + url);
+    result.setSpan(new ForegroundColorSpan(Color.GREEN), 0, label.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+    result.setSpan(new URLSpan(url), label.length() + 2, result.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+    return result;
+  }
+
   @Override
   public List<UIDirective> parse(final String text) {
-    return Collections.<UIDirective>singletonList(new TextViewDirective(text));
+    final ArrayList<UIDirective> result = new ArrayList<UIDirective>();
+    final Matcher m = TECH_SUPPORT_PATTERN.matcher(text);
+    if (m.matches()) {
+      result.add(new TextViewDirective(url("Tech support", m.group(1))));
+    }
+    return result;
   }
 
 }
--- trunk/eclipse-project/src/com/stanfy/arduino/ui/builder/Parser.java
+++ trunk/eclipse-project/src/com/stanfy/arduino/ui/builder/Parser.java
@@ -7,6 +7,8 @@ import java.util.List;
  */
 public interface Parser {
 
+  boolean DEBUG = true;
+
   List<UIDirective> parse(final String text);
 
 }
