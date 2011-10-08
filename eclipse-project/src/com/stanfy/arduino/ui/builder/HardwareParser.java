50f9dd964e5acb8e7c709131716dd594433a537d
Olexandr <rivne2@gmail.com>
2011-10-08 14:10:38 +0000

    Updated parsers

--- eclipse-project/src/com/stanfy/arduino/ui/builder/HardwareParser.java
+++ eclipse-project/src/com/stanfy/arduino/ui/builder/HardwareParser.java
@@ -18,9 +18,10 @@ import android.text.style.URLSpan;
  */
 public class HardwareParser implements Parser {
 
-  private static final Pattern TECH_SUPPORT_PATTERN = Pattern.compile("^Technical\\sSupport:\\s(http://.+)$", Pattern.MULTILINE | Pattern.DOTALL);
+  private static final Pattern TECH_SUPPORT_PATTERN = Pattern.compile("^Technical\\sSupport:\\s(http://.+)", Pattern.MULTILINE | Pattern.DOTALL);
+  private static final Pattern TECH_SUPPORT_PATTERN2 = Pattern.compile("^Technical\\s*Support:\\s*(http://.+)", Pattern.MULTILINE | Pattern.DOTALL);
   private static final Pattern PATTERN1 = Pattern.compile("^.*$", Pattern.MULTILINE);
-  private static final Pattern PATTERN2 = Pattern.compile(".*", Pattern.MULTILINE);
+  private static final Pattern PATTERN2 = Pattern.compile("^.*", Pattern.MULTILINE | Pattern.DOTALL);
   private static final Pattern COMPILED_PATTERN = Pattern.compile("Compiled\\s(.+)\\sby\\s(\\w+)\\n");
   private static final Pattern PROCESSOR_PATTERN = Pattern.compile("Cisco\\s*([a-zA-Z0-9]*\\s*(\\([a-zA-Z0-9]*\\))?)\\s*processor.*with\\s*([0-9\\.]*[kKmMgGtTpP]*)\\s*([a-zA-Z0-9]*)\\s*.*\\n");
 
@@ -49,6 +50,10 @@ public class HardwareParser implements Parser {
   public List<UIDirective> parse(final String text) {
     final ArrayList<UIDirective> result = new ArrayList<UIDirective>();
     Matcher m = TECH_SUPPORT_PATTERN.matcher(text);
+    if (m.matches()) {
+      result.add(new TextViewDirective(url("Tech support", m.group(1))));
+    }
+    m = TECH_SUPPORT_PATTERN2.matcher(text);
     if (m.matches()) {
       result.add(new TextViewDirective(url("Tech support", m.group(1))));
     }
