b6fe37b50f179f1e4d551c75cbedaa0f499ab738
atereshchuk@stanfy.com.ua <atereshchuk@stanfy.com.ua@0f16de9c-53e2-40c1-0ac0-dbf10a6d3398>
2011-10-08 13:43:56 +0000

    Processor parser

--- trunk/eclipse-project/src/com/stanfy/arduino/ui/builder/HardwareParser.java
+++ trunk/eclipse-project/src/com/stanfy/arduino/ui/builder/HardwareParser.java
@@ -6,6 +6,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import android.graphics.Color;
+import android.text.Html;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
@@ -19,6 +20,7 @@ public class HardwareParser implements Parser {
 
   private static final Pattern TECH_SUPPORT_PATTERN = Pattern.compile("Technical\\sSupport:\\s(http://.+)\\n");
   private static final Pattern COMPILED_PATTERN = Pattern.compile("Compiled\\s(.+)\\sby\\s(\\w+)\\n");
+  private static final Pattern PROCESSOR_PATTERN = Pattern.compile("Cisco\\s*([a-zA-Z0-9]*\\s*(\\([a-zA-Z0-9]*\\))?)\\s*processor.*with\\s*([0-9\\.]*[kKmMgGtTpP]*)\\s*([a-zA-Z0-9]*)\\s*.*\\n");
 
   private static Spannable url(final String label, final String url) {
     final SpannableString result = new SpannableString(label + ": " + url);
@@ -35,6 +37,12 @@ public class HardwareParser implements Parser {
     return result;
   }
 
+  private static Spannable processorInfo(final String model, final String memorySize, final String memoryUnits) {
+    final String pattern = "<font color=\"green\">Processor:</font> <font color=\"grey\">%s</font><br/>" +
+    		"<font color=\"green\">Virtual memory:</font> <font color=\"grey\">%s %s</font><br/>";
+    return new SpannableString(Html.fromHtml(String.format(pattern, model, memorySize, memoryUnits)));
+  }
+
   @Override
   public List<UIDirective> parse(final String text) {
     final ArrayList<UIDirective> result = new ArrayList<UIDirective>();
@@ -45,6 +53,10 @@ public class HardwareParser implements Parser {
     m = COMPILED_PATTERN.matcher(text);
     if (m.matches()) {
       result.add(new TextViewDirective(compileInfo(m.group(1), m.group(2))));
+    }
+    m = PROCESSOR_PATTERN.matcher(text);
+    if (m.matches()) {
+      result.add(new TextViewDirective(processorInfo(m.group(1), m.group(3), m.group(4))));
     }
     return result;
   }
